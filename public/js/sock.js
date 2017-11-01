be916b0bd05061d20f5bb6b2bbda9b40def874d6
George Chepiga <egorchepiga@gmail.com>
2017-11-01 11:10:36 +0000

    чиним тесты

--- public/js/sock.js
+++ public/js/sock.js
@@ -1,8 +1,8 @@
-if (socket == "undefined") {
+if (socket == "undefined") { console.log("socket = undefined");
 	if (location.protocol == "https:") {
 	    var _url = location.protocol + "//chat.egorchepiga.ru:3001";
     } else {
 	    var _url = location.protocol + "//chat.egorchepiga.ru:3000";
-    }
+    }};
 	var socket = io.connect(_url);
-};
+
