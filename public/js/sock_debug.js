e6550ea3ad2b5e257977ed769199eccafe8dcb63
George Chepiga <egorchepiga@gmail.com>
2017-11-03 23:43:47 +0000

    правка путей

--- public/js/sock_debug.js
+++ public/js/sock_debug.js
@@ -1,7 +1,7 @@
 if (location.protocol == "https:") {
 	var _url = location.protocol + "//chat.egorchepiga.ru:3001";
 } else {
-	var _url = "http://localhost:3000";
+	var _url = "http://192.168.0.101:3000";
 }
 
 var socket = io.connect(_url);
