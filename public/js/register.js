38a0f887c38668d1f2b8bfe6169f23ba239c2d9c
egorchepiga <egorchepiga@gmail.com>
2017-11-05 23:14:50 +0300

    исправление редиректа после регистрации

--- public/js/register.js
+++ public/js/register.js
@@ -3,18 +3,19 @@ function reg() {
     var lastName = $("input[name='lastname']").val();
     var login = $("input[name='login']").val();
     var pass = $("input[name='password']").val();
+
     _reg(firstName, lastName, login, pass, function (data) {
-        if (data.registration) {
+        if (data.result) {
             document.cookie = `username = ${data.username}`;
             location.reload();
         }
-
     });
 }
 
 function _reg(firstName, lastName, login, pass, callback) {
     socket.emit('register', {login: login, pass: pass, lastName: lastName, firstName: firstName},
         function (data) {
+        console.log(data);
         callback(data);
     });
 }
